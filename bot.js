9c5fe8c2dd40c173a9e8bcaecff161c1536fbcf3
= <51408991+Consolidating@users.noreply.github.com>
2021-03-07 12:54:25 -0500

    fixes

--- bot.js
+++ bot.js
@@ -17,32 +17,13 @@ client.on('message', msg => {
 });
 
 
-// let CareerPageLink = "https://recruiting.ultipro.ca/BEN5000BENTK/JobBoard/6f7f3dc0-d599-4e78-9ee5-fa1703eb67ac/?q=&o=postedDateDesc&w=&wc=&we=&wpst="
-
-// client.on('message', msg => {
-//     if (msg.content === '[Bentall Bot] Careers') {
-//       msg.channel.send('Check out our latest job postings here:' + CareerPageLink);
-//     }
-//   });
-
-
-
 //   client.on('[user info]', msg => {
 //     msg.channel.send(msg.author)
 //     msg.channel.send('Test')
 //   });
 
 
-//   // client.on('message', msg => {
-  //   if (msg.content === '[ban-list-update]') {
-  //     msg.reply('Yees?!');
-  //   }
-  // });
-
-//Ban List
-
-
-// client.on('message', msg => {
+ //client.on('message', msg => {
 
 //   if (msg.content === '[banlist-update]') {
 //     //msg.channel.send("Test")
@@ -54,6 +35,7 @@ client.on('message', msg => {
 //      })
 //   }
 // });
+
 //Ban List
 const cheerio = require('cheerio');
 
@@ -72,53 +54,23 @@ client.on("message", message => {
   };
 });
 
-//Card Info
-//  client.on('message', async msg => {
-//    let prefix = '[card-info]'
-//      const arg = msg.content.replace(prefix,"").trim()
-    
-//      let result = await cardInfo(arg).then(data=>{
-//        const carddata = {
-//               name: data.data[0].name,
-//               card_type: data.data[0].type,
-//               atk: data.data[0].atk,
-//               def: data.data[0].def,
-//               level: data.data[0].level,
-//               monstertype:data.data[0].race,
-//               effect:data.data[0].desc
-//        }
+//Card Data need to refactor and try to get it to stop looping on error...
+ client.on('message',  msg => {
 
-//      return carddata
-//      }).catch((undefined, e)=>{
-//        if (e){
-//          throw e
-//        }
-//      })
-
-//      if (result){
-//        const JSONCard = JSON.stringify(result, null, 2);
-//        msg.channel.send('```json\n' + JSONCard + '\n```')
-//        return false
-//      } 
-    
- 
   
-//  });
+  let prefix = '[card-info]'
+  const arg = msg.content.replace(prefix,"").trim()    
+  getData(arg)
+  
+  async function getData(arg){
+    let result = await cardInfo(arg)
+    if(result){
+      return msg.channel.send('```json\n' + result + '\n```')
+    }
 
 
-
-// const cardInfotest  = require('./util/test');
-
-// client.on('message', async msg => {
-//   let prefix = '[card-info]'
-//   const arg = msg.content.replace(prefix,"").trim()
-    
-//     let result = await cardInfotest(arg)
-
-//        return msg.channel.send('```json\n' + result + '\n```')
-//        console.log('mesage')
-
-// });
+  }})
+  
 
 
 
