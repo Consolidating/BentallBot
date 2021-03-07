dc03fac55582fb59defe46fda175fae340f15018
= <51408991+Consolidating@users.noreply.github.com>
2021-03-07 01:16:44 -0500

    Changes

--- bot.js
+++ bot.js
@@ -17,23 +17,23 @@ client.on('message', msg => {
 });
 
 
-let CareerPageLink = "https://recruiting.ultipro.ca/BEN5000BENTK/JobBoard/6f7f3dc0-d599-4e78-9ee5-fa1703eb67ac/?q=&o=postedDateDesc&w=&wc=&we=&wpst="
+// let CareerPageLink = "https://recruiting.ultipro.ca/BEN5000BENTK/JobBoard/6f7f3dc0-d599-4e78-9ee5-fa1703eb67ac/?q=&o=postedDateDesc&w=&wc=&we=&wpst="
 
-client.on('message', msg => {
-    if (msg.content === '[Bentall Bot] Careers') {
-      msg.channel.send('Check out our latest job postings here:' + CareerPageLink);
-    }
-  });
-
-
-
-  client.on('[user info]', msg => {
-    msg.channel.send(msg.author)
-    msg.channel.send('Test')
-  });
+// client.on('message', msg => {
+//     if (msg.content === '[Bentall Bot] Careers') {
+//       msg.channel.send('Check out our latest job postings here:' + CareerPageLink);
+//     }
+//   });
 
 
-  // client.on('message', msg => {
+
+//   client.on('[user info]', msg => {
+//     msg.channel.send(msg.author)
+//     msg.channel.send('Test')
+//   });
+
+
+//   // client.on('message', msg => {
   //   if (msg.content === '[ban-list-update]') {
   //     msg.reply('Yees?!');
   //   }
@@ -73,37 +73,37 @@ client.on("message", message => {
 });
 
 //Card Info
- client.on('message', async msg => {
-   let prefix = '[card-info]'
-     const arg = msg.content.replace(prefix,"").trim()
+//  client.on('message', async msg => {
+//    let prefix = '[card-info]'
+//      const arg = msg.content.replace(prefix,"").trim()
     
-     let result = await cardInfo(arg).then(data=>{
-       const carddata = {
-              name: data.data[0].name,
-              card_type: data.data[0].type,
-              atk: data.data[0].atk,
-              def: data.data[0].def,
-              level: data.data[0].level,
-              monstertype:data.data[0].race,
-              effect:data.data[0].desc
-       }
+//      let result = await cardInfo(arg).then(data=>{
+//        const carddata = {
+//               name: data.data[0].name,
+//               card_type: data.data[0].type,
+//               atk: data.data[0].atk,
+//               def: data.data[0].def,
+//               level: data.data[0].level,
+//               monstertype:data.data[0].race,
+//               effect:data.data[0].desc
+//        }
 
-     return carddata
-     }).catch((undefined, e)=>{
-       if (e){
-         throw e
-       }
-     })
+//      return carddata
+//      }).catch((undefined, e)=>{
+//        if (e){
+//          throw e
+//        }
+//      })
 
-     if (result){
-       const JSONCard = JSON.stringify(result, null, 2);
-       msg.channel.send('```json\n' + JSONCard + '\n```')
-       return false
-     } 
+//      if (result){
+//        const JSONCard = JSON.stringify(result, null, 2);
+//        msg.channel.send('```json\n' + JSONCard + '\n```')
+//        return false
+//      } 
     
  
   
- });
+//  });
 
 
 
