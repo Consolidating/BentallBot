5b060a3cf756f4381bb828082481640544ad1a15
= <51408991+Consolidating@users.noreply.github.com>
2021-03-07 12:51:16 -0500

    Test

--- util/ygo.js
+++ util/ygo.js
@@ -5,18 +5,30 @@ function banlist() {
     return axios.get(url).then(response => response.data)
 }
 
-function cardInfo(cardname){
-    const ygourl = `https://db.ygoprodeck.com/api/v7/cardinfo.php?&name=${cardname}`
-    return axios.get(ygourl).then(response => response.data)
+async function cardinfo(cardname){
+    const ygourl = `https://db.ygoprodeck.com/api/v7/cardinfo.php?&fname=${cardname}`
+    const data = await axios.get(ygourl).then(response => response.data).catch(()=>{})
+
+   if(data){
+    const carddata = {
+        name: data.data[0].name,
+        card_type: data.data[0].type,
+        atk: data.data[0].atk,
+        def: data.data[0].def,
+        level: data.data[0].level,
+        monstertype:data.data[0].race,
+        effect:data.data[0].desc
+    }
+        const JSONCard = JSON.stringify(carddata, null, 2);
+        return JSONCard
+   } 
 
 }
-
-//cardInfo("dark magician")
 
 
 module.exports = {
     banlist: banlist,
-    cardInfo: cardInfo,
+    cardInfo: cardinfo,
 }
 
 
